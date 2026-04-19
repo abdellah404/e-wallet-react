9ad9cbe4eb2397490bf4f6d23efe4a79ae5a28a9
abdellah404 <1234abdellah007@gmail.com>
2026-04-19 23:38:27 +0000

    test

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Header from './header'
+import Footer from './footer'
+import Index from './Index'
+import Login from './login'
+import Dashboard from './dashboard'
+import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <Dashboard />
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,22 @@
+
+
+
+function Header() {
+    return (
+        
+    <nav className="navbar">
+      <a href="#" className="logo">
+        <img src="../src/assets/e-wallet-logo.avif" alt="E-Wallet Logo"/>
+      </a>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+ 
+       
+    );
+}
+export default Header;
